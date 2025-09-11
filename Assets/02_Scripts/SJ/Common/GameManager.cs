c468e89d1889db434b7415f92d10fdde85dfc458
9rasshopper <tmdwns0615@gmail.com>
2025-09-11 13:50:19 +0900

    패널 생성
    임시로 사용하는 패널 기능

--- Assets/02_Scripts/SJ/Common/GameManager.cs
+++ Assets/02_Scripts/SJ/Common/GameManager.cs
@@ -1,52 +1,42 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager>
 {
-    [SerializeField] private GameObject confirmPrefab;           // 확인 패널
-    [SerializeField] private GameObject signinPrefab;            // 로그인 패널
-    [SerializeField] private GameObject signupPrefab;            // 회원가입 패널
-    [SerializeField] private GameObject settingPrefab;            // 설정 패널
-    [SerializeField] private GameObject rankingPrefab;            // 랭킹 패널
-    //각 패널은 프리팹으로 만들어 어떤 상황에서도 동작하게 만들기
-    private GameObject settingPanel;
+    [SerializeField] private GameObject confirmPanel;           // 확인 패널
+    [SerializeField] private GameObject signinPanel;            // 로그인 패널
+    [SerializeField] private GameObject signupPanel;            // 회원가입 패널
+    [SerializeField] private GameObject rankingPanel;            // 랭킹 패널
     
     public static Constants.GameType _gameType;
     
     private Canvas _canvas;
-    
-    void Start()
+
+    void Awake()
     {
-        _canvas =  GameObject.Find("Canvas").GetComponent<Canvas>();
+        _canvas = FindFirstObjectByType<Canvas>();
     }
 
-    void Update()
-    {
-        if (Keyboard.current.escapeKey.wasPressedThisFrame)
-        {
-            if (settingPanel == null)
-            {
-                settingPanel = Instantiate(settingPrefab, _canvas.transform);
-                settingPanel.SetActive(true);
-            }
-            else
-                settingPanel.SetActive(!settingPanel.activeSelf);
-        }
-    }
     public void ChangeToGameScene(Constants.GameType gameType)
     {
         _gameType = gameType;
         SceneManager.LoadScene("test_game"); //임시로 테스트 씬으로 이동
     }
 
+    public void OpenconfirmPanel()
+    {
+        Instantiate(confirmPanel, _canvas.transform);
+    }
+    
     protected override void OnSceneLoad(Scene scene, LoadSceneMode mode)
     {
-        _canvas = FindFirstObjectByType<Canvas>();
-        if (scene.name == "")
+        //_canvas = FindFirstObjectByType<Canvas>();
+        /*if (scene.name == "")
         {
             
             
-        }
+        }*/
     }
 }
