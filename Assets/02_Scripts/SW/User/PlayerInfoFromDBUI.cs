7906bf994aa994f60f140aea0b2d43cf91d456d8
Jeong-Hayoon <137061867+Jeong-Hayoon@users.noreply.github.com>
2025-09-18 11:05:24 +0900

    흑돌/백돌 순서 정함에 따라 게임씬에서의 닉네임, 급수 지정하는 방식으로 변경

--- Assets/02_Scripts/SW/User/PlayerInfoFromDBUI.cs
+++ Assets/02_Scripts/SW/User/PlayerInfoFromDBUI.cs
@@ -17,16 +17,41 @@ public class PlayerInfoFromDBUI : MonoBehaviour
         // 게임 모드에 따라 상대 정보 다르게 세팅
         if (GameManager._gameType == Constants.GameType.SinglePlay)
         {
-            // 싱글플레이 → 상대는 AI, 같은 급수
-            playerBNicknameText.text = "AI";
-            playerBRankText.text = $"{UserData.Instance.Rank}급";
+            if (!GameManager.Instance.isSwitched)
+            {
+                // 싱글플레이 → 상대는 AI, 같은 급수
+                playerBNicknameText.text = "AI";
+                playerBRankText.text = $"{UserData.Instance.Rank}급";
+            }
+            else
+            {
+                playerANicknameText.text = "AI";
+                playerARankText.text = $"{UserData.Instance.Rank}급";
+            }
         }
-        else if(GameManager._gameType == Constants.GameType.DualPlay)
+        else if (GameManager._gameType == Constants.GameType.DualPlay)
         {
+            if (!GameManager.Instance.isSwitched)
+            {
 
+            }
+            else
+            {
+
+
+            }
         }
         else
         {
+            if (!GameManager.Instance.isSwitched)
+            {
+
+            }
+            else
+            {
+
+
+            }
             // 멀티플레이일 경우 → 초기값
             // 아직 미구현이라 닉네임, 랭크 따로 세팅 안함
             playerBNicknameText.text = "???";
